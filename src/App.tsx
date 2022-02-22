26b9d06c99f611ddf385074d3208f6f2e5638bdc
Pavel Gritsenko <pdudarevp@gmail.com>
2022-02-22 17:03:53 +0300

    feat: add initialization app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
+import React from "react";
 import './App.scss';
 import Header from "./components/Header/Header";
 import {Routes, Route} from "react-router-dom"
 import Login from "./components/Body/Login/Login";
 import Coins from "./components/Body/Coins/Shop/Coins";
 import Wallet from "./components/Body/Coins/Wallet/Wallet";
+import {useTypedSelector} from "./hooks/useTypedSelector";
 
 function App() {
+  const {isLogin} = useTypedSelector(state => state.auth)
+
+  if (!isLogin) {
+    return (
+      <div className="wrapper">
+        <Header />
+        <div className="body">
+          <Routes>
+            <Route path="*" element={<Login />}/>
+            <Route path="/login" element={<Login />}/>
+          </Routes>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="wrapper">
       <Header />
